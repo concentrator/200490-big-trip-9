4d0a9cbb22639d65fc69c81e4edc8fb8de3fd809
Dmitry Iovlev <iovlev.d@gmail.com>
2019-08-21 14:35:37 +0300

    Добавляет функцию генерации моков событий

--- src/data.js
+++ src/data.js
@@ -109,6 +109,89 @@ const TripDays = [
 
 // ];
 
+const getRandomBoolean = () => Boolean(Math.round(Math.random()));
+
+const shuffleArray = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+};
+
+const getRandomDescription = () => {
+  return shuffleArray([
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+    `Fusce tristique felis at fermentum pharetra.`,
+    `Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`, `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+    `Aliquam erat volutpat.`,
+    `Nunc fermentum tortor ac porta dapibus.`,
+    `In rutrum ac purus sit amet tempus.`
+  ]).slice(0, Math.ceil(Math.random() * 3)).join(` `);
+};
+
+const getEvent = () => ({
+  type: [
+    `taxi`,
+    `bus`,
+    `train`,
+    `ship`,
+    `transport`,
+    `drive`,
+    `flight`,
+    `check`,
+    `sightseeing`,
+    `restaurant`
+  ][Math.floor(Math.random() * 10)],
+  destination: [
+    `Amsterdam`,
+    `Geneva`,
+    `Chamonix`,
+    `Saint Petersburg`
+  ][Math.floor(Math.random() * 3)],
+  photos: [
+    `http://picsum.photos/300/150?r=${Math.random()}`,
+    `http://picsum.photos/300/150?r=${Math.random()}`,
+    `http://picsum.photos/300/150?r=${Math.random()}`,
+    `http://picsum.photos/300/150?r=${Math.random()}`,
+    `http://picsum.photos/300/150?r=${Math.random()}`
+  ],
+  description: getRandomDescription(),
+  price: Math.ceil(Math.random() * 10) * 20,
+  offers: shuffleArray([
+    {
+      title: `Add luggage`,
+      price: 10,
+      isChecked: getRandomBoolean()
+    },
+    {
+      title: `Switch to comfort class`,
+      price: 150,
+      isChecked: getRandomBoolean()
+    },
+    {
+      title: `Add meal`,
+      price: 2,
+      isChecked: getRandomBoolean()
+    },
+    {
+      title: `Choose seats`,
+      price: 9,
+      isChecked: getRandomBoolean()
+    }
+  ]).slice(0, Math.floor(Math.random() * 3))
+});
+
+const event = getEvent();
+
+console.log(event);
+
 const data = {
   MenuItems,
   FilterItems,
